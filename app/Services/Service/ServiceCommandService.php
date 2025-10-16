2fd5ddbd786c41b02bb6e6960ac5b295921d22a8
ersatama <ersatama@gmail.com>
2025-10-16 16:30:06 +0500

    feat: added booking and service CQRS services

---
+++ app/Services/Service/ServiceCommandService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Service;
+
+use App\Services\CommandService;
+
+class ServiceCommandService extends CommandService
+{
+
+}
---
+++ app/Services/Service/ServiceQueryService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Service;
+
+use App\Services\QueryService;
+
+class ServiceQueryService extends QueryService
+{
+
+}
