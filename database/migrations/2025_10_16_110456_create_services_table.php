c29123fff78f12954aa5923ea8e846754ae727d9
ersatama <ersatama@gmail.com>
2025-10-16 16:11:56 +0500

    feat: created migrations for services & bookings table

---
+++ database/migrations/2025_10_16_110456_create_services_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('duration_minutes');
+            $table->json('working_days')->nullable();
+            $table->time('work_start')->default('10:00');
+            $table->time('work_end')->default('20:00');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
